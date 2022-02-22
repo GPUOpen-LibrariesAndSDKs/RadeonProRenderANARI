ee7e03f96f3b2b5e3dd6319ba49935f24bcb9c73
V-Gorash <VGorash@luxoft.com>
2022-02-22 17:00:06 +0300

    density lookup

--- scene/volume/ScivisVolume.h
+++ scene/volume/ScivisVolume.h
@@ -15,12 +15,15 @@ public:
 private:
   void clear();
   mat4 getVolumeTransform() override;
+  static float32 processDensity(float32 index, Array1D* densityLookup, rpr_image_filter_type filter);
 
   rpr_image m_color_map{};
   rpr_material_node m_color_material{};
-  rpr_material_node m_density_material{};
-  rpr_grid m_grid{};
-  mat4 m_transform;
+  rpr_material_node m_color_grid_material{};
+  rpr_material_node m_density_grid_material{};
+  rpr_grid m_color_grid{};
+  rpr_grid m_density_grid{};
+  mat4 m_transform{};
 };
 
 }
